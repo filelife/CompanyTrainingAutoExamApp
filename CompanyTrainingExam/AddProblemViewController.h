d8fd8be5b8ca6bac0b401591b2a7181be9120a89
Gejiaxin <filelife_g@hotmail.com>
2017-02-10 22:07:22 +0800

    update

---
+++ CompanyTrainingExam/AddProblemViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddProblemViewController.h
+//  CompanyTrainingExam
+//
+//  Created by Gejiaxin on 17/2/10.
+//  Copyright © 2017年 VincentJac. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AddProblemViewController : NSViewController
+
+@end
---
+++ CompanyTrainingExam/AddProblemViewController.m
@@ -0,0 +1,22 @@
+//
+//  AddProblemViewController.m
+//  CompanyTrainingExam
+//
+//  Created by Gejiaxin on 17/2/10.
+//  Copyright © 2017年 VincentJac. All rights reserved.
+//
+
+#import "AddProblemViewController.h"
+
+@interface AddProblemViewController ()
+
+@end
+
+@implementation AddProblemViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
